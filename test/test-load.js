be453560a52e816670abedfd8bad209cfbe733ff
Ryan Graham <r.m.graham@gmail.com>
2016-05-05 17:27:54 -0700

    update copyright notices and license

--- test/test-load.js
+++ test/test-load.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2014. All Rights Reserved.
+// Node module: generator-bacn
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 /*global describe, beforeEach, it*/
 'use strict';
 
